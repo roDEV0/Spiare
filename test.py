bccc9680594f2734d16b2ff5b3065230b7c98948
roDEV0 <gian@romanodev.com>
2026-05-26 09:45:20 -0400

    Proper env injection

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-from PIL import Image
-from io import BytesIO
-
-response = requests.get("https://map.earthmc.net/tiles/minecraft_overworld/0/-5_0.png")
-img = Image.open(BytesIO(response.content))
-img.show()
