fc5d7c9561e4ede1efe413b0db8182faf7906f2c
roDEV0 <gian@romanodev.com>
2026-05-26 21:37:16 -0400

    Stop console flooding lol

--- watcher/jobs/objective.py
+++ watcher/jobs/objective.py
@@ -255,7 +255,6 @@ async def clean_dead_sessions(requester):
 
 async def get_valid_data(requester, category, get_list, retries=3):
     check_list = await requester.post_request_batch(category, get_list)
-    print(check_list)
     if len(check_list) != len(get_list):
         if retries <= 0:
             print(f"{len(get_list) - len(check_list)} objects were unable to be fetched")
